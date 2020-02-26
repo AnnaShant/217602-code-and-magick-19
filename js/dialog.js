57f4383cf914e99702d12ba5a3c58e1e82d51000
AnnaShant <anna.lunatic.t@gmail.com>
2020-02-26 22:48:19 +0500

    Задание 6

--- js/dialog.js
+++ js/dialog.js
@@ -4,6 +4,7 @@
 (function () {
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = document.querySelector('.setup-close');
+  var formWizard = window.setup.setupWizard.querySelector('.setup-wizard-form');
 
   var onPopupEscPress = function (evt) {
     window.util.isEscEvent(evt, closePopup);
@@ -34,4 +35,31 @@
     window.setup.setupWizard.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
   };
+
+  // Отправка формы
+  var sendForm = function () {
+    window.setup.setupWizard.classList.add('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var onFormSubmit = function (evt) {
+    evt.preventDefault();
+
+    window.backend.save(new FormData(formWizard), sendForm);
+  };
+
+  formWizard.addEventListener('submit', onFormSubmit, errorHandler);
 })();
